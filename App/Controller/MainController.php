947ff8607e19edeaf302fdcfcf72d171a66cd6e2
= <=>
2020-12-15 15:50:32 +0100

    first routing steps

--- App/Controller/MainController.php
+++ App/Controller/MainController.php
@@ -7,30 +7,8 @@ class MainController{
         $this->model = new MainModel();
     }
 
-    public function main(){
+    public function render(){
 
-        // Set the current date 
-        date_default_timezone_set("Europe/Paris");
-        $currentDateTime = date('Y-m-d H:i:s');
-        // Get all user's poll 
-        $exeAllPolls = $this->model->getAlluserPolls($_SESSION['id'],$currentDateTime);
-        // Get the user name 
-        $user = $this->model->getUserName($_SESSION["id"]);
-        // Get the user's friends list 
-        $friendsId = $this->model->friendsIdList($_SESSION["id"]);
-        $friendsPolls = [];
-
-        // Friends polls
-        for($i = 0; $i<count($friendsId); $i++){
-            $currentFriend = $friendsId[$i]->friend_id;
-            $friendCurrentName = $this->model->friendName($currentFriend);
-            date_default_timezone_set("Europe/Paris");
-            $currentDateTime = date('Y-m-d H:i:s');
-
-            // Choose only polls form friends if the limit date of the poll isnt passed 
-            $friendCurrentPolls = $this->model->currentFriendsPolls($currentFriend, $currentDateTime);
-            array_push($friendsPolls, $friendCurrentPolls);
-        }
         require ROOT."/App/View/MainView.php";
+        }
     }
-}
--- Rooter.php
+++ Rooter.php
@@ -2,6 +2,15 @@
 use App\Controller\MainController;
 
 
+if (array_key_exists("page", $_GET)) {
 
-
-?>
+    switch ($_GET["page"]) {
+        case 'main':
+           
+        }
+        //display default page
+    }else{
+        $controller = new MainController();
+        $controller->render();
+    }
+    ?>
