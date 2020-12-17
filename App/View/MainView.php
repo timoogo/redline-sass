10d74b0f53cd6b2a9a2747dbcd5398c7535aaf6c
= <=>
2020-12-17 18:53:38 +0100

    stats main page

--- App/View/MainView.php
+++ App/View/MainView.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,43 +8,46 @@
     <link rel="stylesheet" href="assets/css/import.css">
     <script src="https://kit.fontawesome.com/2a28e9e3fb.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-   <?php include ROOT. "/public/includes/_header.php" ;?>
+    <?php include ROOT. "/public/includes/_header.php" ;?>
     <main>
         <section class="hero--centered">
             <h2 class="catch-phrase">
-            Participez à des sondages en <span class="highlight">temps réel</span> et comparez vos résultats
+                Participez à des sondages en <span class="highlight">temps réel</span> et comparez vos résultats
             </h2>
-           <a class="button"href="#">Regarder un exemple</a>
+            <a class="button" href="#">Regarder un exemple</a>
         </section>
         <section singlePoll>
             <div class="title">
-                <h2>Les céréales, <span class="highlight">avant</span> ou <span class="highlight">après</span> le lait ?</h2>
-                <span class="highlight">Demandé par <span class="highlight clear">timogo</span> il y a une semaine</span>
+                <h2>Les céréales, <span class="highlight">avant</span> ou <span class="highlight">après</span> le lait ?
+                </h2>
+                <span class="highlight">Demandé par <span class="highlight clear">timogo</span> il y a une
+                    semaine</span>
             </div>
             <div class="details">
-             <div class="cards">
-             <div class="card answer">
-                    <div class="number">3</div>
-                    <h4 class="choice">Avant</h4>
-                    <div class="progress">
-                        <progress value='1'max='2'></progress>
+                <div class="cards">
+                    <div class="card answer">
+                        <div class="number">3</div>
+                        <h4 class="choice">Avant</h4>
+                        <div class="progress">
+                            <progress value='1' max='2'></progress>
+                        </div>
+                    </div>
+                    <div class="card answer">
+                        <div class="number">3</div>
+                        <h4 class="choice">Avant</h4>
+                        <div class="progress">
+                            <progress value='1' max='2'></progress>
+                        </div>
                     </div>
                 </div>
-                <div class="card answer">
-                    <div class="number">3</div>
-                    <h4 class="choice">Avant</h4>
-                    <div class="progress">
-                        <progress value='1'max='2'></progress>
-                    </div>
-                </div>
-             </div>
                 <aside>
                     <button>Répondre</button>
                     <h4>Nb de vote</h4>
-                   <div class="vote">
-                   <span>4</span>
-                   </div>
+                    <div class="vote">
+                        <span>4</span>
+                    </div>
                     <section class="social-links">
                         <a href="#"><i class="fab fa-github"></i></a>
                         <a href="#"><i class="fab fa-github"></i></a>
@@ -55,13 +59,24 @@
         </section>
         <section class="hero--compare">
             <div class="content">
-            <h2>Comparez vous à vos <span class="highlight">amis</span> en quelques <span class="highlight">clics</span> !</h2>
-        <h3>Répondez aux <span class="highlight">sondages</span> en quelques clics puis demandez <span class="highlight">l'avis</span> à vos amis en le partageant par <span class="highlight">Messenger</span>, <span class="highlight">Twitter</span>, <span class="highlight">SMS</span> et <span class="highlight">WhatsApp</span>. Vous serez sans doute supris de l’avis de vos amis !</h3>
+                <h2>Comparez vous à vos <span class="highlight">amis</span> en quelques <span
+                        class="highlight">clics</span> !</h2>
+                <h3>Répondez aux <span class="highlight">sondages</span> en quelques clics puis demandez <span
+                        class="highlight">l'avis</span> à vos amis en le partageant par <span
+                        class="highlight">Messenger</span>, <span class="highlight">Twitter</span>, <span
+                        class="highlight">SMS</span> et <span class="highlight">WhatsApp</span>. Vous serez sans doute
+                    supris de l’avis de vos amis !</h3>
             </div>
         </section>
 
 
         <section preview>
+            <section class="social-links">
+                <a href="#"><i class="fab fa-github"></i></a>
+                <a href="#"><i class="fab fa-github"></i></a>
+                <a href="#"><i class="fab fa-github"></i></a>
+                <a href="#"><i class="fab fa-github"></i></a>
+            </section>
             <h2 class="clear">Regardez vos stats</h2>
             <section MyApp>
                 <header>
@@ -76,35 +91,36 @@
                     <h4>Dashboard</h4>
                     <span class="annotation">Ici tu verras tes stats </span>
                 </div>
-              
+
                 <section class="cards">
                     <div class="card profil--response">
                         <div class="smiley"><img src="assets/img/smiley.png" alt="smiley"></div>
                         <h4>Réponses correctes</h4>
                         <div class="progress">
-                          <progress value='1'max='2'></progress>
-                    </div>
-                    </div>
-                    <div class="card profil--response">
-                        <div class="smiley"><img src="assets/img/smiley.png" alt="smiley"></div>
-                        <h4>Réponses correctes</h4>
-                        <div class="progress">
-                          <progress value='1'max='2'></progress>
-                    </div>
+                            <progress value='1' max='2'></progress>
+                        </div>
                     </div>
                     <div class="card profil--response">
                         <div class="smiley"><img src="assets/img/smiley.png" alt="smiley"></div>
                         <h4>Réponses correctes</h4>
                         <div class="progress">
-                          <progress value='1'max='2'></progress>
+                            <progress value='1' max='2'></progress>
+                        </div>
                     </div>
+                    <div class="card profil--response">
+                        <div class="smiley"><img src="assets/img/smiley.png" alt="smiley"></div>
+                        <h4>Réponses correctes</h4>
+                        <div class="progress">
+                            <progress value='1' max='2'></progress>
+                        </div>
                     </div>
                 </section>
             </section>
-        </section>
+            </div>
     </main>
 
 
-   <?php include ROOT. "/public/includes/_footer.php" ;?>
+    <?php include ROOT. "/public/includes/_footer.php" ;?>
 </body>
+
 </html>
